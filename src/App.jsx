d204e509b9bea9c139fdd68efdf6366818141c17
Shreya <shreya186btcse23@igdtuw.ac.in>
2024-09-23 10:54:38 +0530

    Home Page done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <Home />
+    </div>
+  )
+}
+
+export default App;
+
+
